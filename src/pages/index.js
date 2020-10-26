f3295172d811868c9c7d7f39d637d6485e469400
vsavard <sav.vero@gmail.com>
2020-10-26 19:01:14 +0000

    added button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,32 @@
 import React from "react"
 import Container from "@material-ui/core/Container"
 import CssBaseline from "@material-ui/core/CssBaseline"
-import picture from "../../static/assets/verobw.png"
+import Button from '@material-ui/core/Button';
 import { Link } from "gatsby"
+import picture from "../../static/assets/verobw.png"
 
 const imgStyle = {
-  margin: "10% 33%",
+  margin: "10% 25%",
   border: "2px solid black",
   borderRadius: "50%",
   maxWidth: "300px",
 }
+
 
 export default () => {
   return (
     <>
       {" "}
       <CssBaseline />
-      <Container maxWidth="sm">
+      <Container maxWidth="sm" >
         {" "}
-        <h1>Welcome to my HSA Journal!</h1>
+        <h2>Welcome to my HSA Journal!</h2>
         <img src={picture} alt="Veronique" style={imgStyle}></img>
-        <p>Profile: VS, student</p>
-        <h3>
+        <p>Profile: Veronique Savard</p>
+        <p>2020/2021 HSA Researching Interactions with Technology</p>
+        <Button variant="outlined" color="secondary">
           <Link to="/blog">View Blog</Link>
-        </h3>
+        </Button>
         {/* <p><Link to="/account">My Account</Link></p> */}
       </Container>
     </>
